468908ebee4b6ec11f848b5562d1179d358eceb8
Selçuk <110834643+selcukgiray0000@users.noreply.github.com>
2022-08-21 18:45:55 +0300

    Update editblog.php

--- blog/editblog.php
+++ blog/editblog.php
@@ -45,7 +45,7 @@ if($_POST)
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Hello, world!</title>
+    <title>Edit Blog</title>
   </head>
   <body>
     <?php include "../navbar.php"?>
